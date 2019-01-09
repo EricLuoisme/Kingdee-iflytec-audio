d38ae724b6747e4745a524f12fa6aafee3f7fcc1
u6270944 <u6270944@anu.edu.au>
2019-01-09 10:13:23 +0800

    Recover Old Version of Audio-Processing Program

--- AudioProcess/Script_process.py
+++ AudioProcess/Script_process.py
@@ -1,16 +1,24 @@
 import time
 
 from AudioProcess.data import WakeWord_Setting, Timing_Setting
-from AudioProcess import Main_process, Script_General_Ledger, Script_Receivable_module
-from NAudioProcess import NStatus
+from AudioProcess import Main_process, Recognition_process, Script_General_Ledger, Script_Receivable_module
 
 
 def arouse_recognize(result):
-
-    if result.__contains__(WakeWord_Setting.wake_word):
-        return NStatus('Arousing')
+    if result is not "":
+        if result.__contains__(WakeWord_Setting.wake_word):
+            print("您好，小智来了")
+            Recognition_process.doing_voice_Comb("您好，小智来了")
+            tt = Main_process.TapTester()
+            time.sleep(1)
+            print('开始接收指令')
+            for ii in range(Timing_Setting.script_record_time):
+                check = tt.listen(2)
+                if check:
+                    # 正确执行指令后，check为True
+                    return True
     else:
-        return None
+        return False
 
 
 def script_recognize(result):
@@ -20,26 +28,22 @@ def script_recognize(result):
 
         # 错误唤醒 操作
         if result.__contains__("没事"):
-            return NStatus('Mis_operation')
+            return True
 
         # 制作/生成 操作
         if result.__contains__("制作") or result.__contains__("生成"):
             # 总账场景 2 + 应收场景 1
             check = Script_General_Ledger.do_script(result, 2)  # 完成执行操作后，check应返回False
             if check is False:
-                return NStatus('General_Ledger', 2)
+                return True
 
             check = Script_Receivable_module.do_script(result, 1)  # 完成执行操作后，check应返回False
             if check is False:
-                return NStatus('Receivable_Module', 1)
+                return True
 
         # 处理/审核 操作
         elif result.__contains__("处理"):
             # 总账场景 3,5 + 应收场景 2
-
-            # 子场景需要回调 调用的函数
-            # 需要得到具体函数形式才能对此块进行修改
-
             check = Script_General_Ledger.do_script(result, 3)  # 完成执行操作后，check应返回False
             if check is False:
                 mode_3_trigger('g', 3)  # 这边存在子场景
@@ -48,7 +52,6 @@ def script_recognize(result):
             if check is False:
                 mode_3_trigger('g', 5)  # 这边存在子场景
                 return True
-
             check = Script_Receivable_module.do_script(result, 2)  # 完成执行操作后，check应返回False
             if check is False:
                 mode_3_trigger('r', 2)  # 这边存在子场景
@@ -59,15 +62,15 @@ def script_recognize(result):
             # 总账场景 4,6 + 应收场景 3
             check = Script_General_Ledger.do_script(result, 4)  # 完成执行操作后，check应返回False
             if check is False:
-                return NStatus('General_Ledger', 4)
+                return True
             check = Script_General_Ledger.do_script(result, 6)  # 完成执行操作后，check应返回False
             if check is False:
-                return NStatus('General_Ledger', 6)
+                return True
             check = Script_Receivable_module.do_script(result, 3)  # 完成执行操作后，check应返回False
             if check is False:
-                return NStatus('Receivable_Module', 3)
+                return True
     else:
-        return None
+        return False
 
 
 def mode_3_trigger(scenario, scenario_number):
@@ -87,10 +90,10 @@ def sub_script_recognize(result, sub_scenario, sub_scenario_number):
     if result.__contains__("好") or result.__contains__("是") or result.__contains__("可以"):
         # 执行查看原因操作
         if sub_scenario is 'g':
-            Script_General_Ledger.do_sub_script(result, sub_scenario_number)
+            Script_General_Ledger.do_sub_script(sub_scenario_number)
         else:
             # 'r'
-            Script_Receivable_module.do_sub_script(result, sub_scenario_number)
+            Script_Receivable_module.do_sub_script(sub_scenario_number)
         return True
     elif result.__contains__("不"):
         # 执行取消操作
